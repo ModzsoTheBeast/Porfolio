8552db8a4da5273242032c04e5a23cd8a7285af3
Lőkös Patrik <67261375+ModzsoTheBeast@users.noreply.github.com>
2022-12-04 10:19:35 +0100

    asd
    asd

--- portfolio/src/app/components/cursor/cursor.component.ts
+++ portfolio/src/app/components/cursor/cursor.component.ts
@@ -24,6 +24,35 @@ export class CursorComponent implements OnInit {
     }, 50)
   }
 
+/*  @HostListener("document:mouseover", ['$event'])
+  onMouseOver(event:MouseEvent){
+    console.log(event)
+    str(event)
+    let value = ''
+    value += event.type + ':  ' +
+      'target=' + str(event.target) +
+      ',  relatedTarget=' + str(event.relatedTarget) + "\n";
+    console.log(value)
 
+    this.circle.nativeElement.style.width = 100 + 'px'
+    this.circle.nativeElement.style.height = 100 + 'px'
+  }
 
+  @HostListener("document:mouseout", ['$event'])
+  onMouseOut(event:any){
+    str(event)
+    let value = ''
+    value += event.type + ':  ' +
+      'target=' + str(event.target) +
+      ',  relatedTarget=' + str(event.relatedTarget) + "\n";
+    console.log(value)
+    this.circle.nativeElement.style.width = ""
+    this.circle.nativeElement.style.height = ""
+  }*/
 }
+
+function str(el: any) {
+  if (!el) return "null"
+  return el.className || el.tagName;
+}
+
